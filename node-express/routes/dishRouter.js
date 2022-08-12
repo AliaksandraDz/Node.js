2fcdcac1ad15f39bc6dd295450d198f9b90154b0
Alexandra <wilderweinundich@gmail.com>
2022-08-12 19:22:52 +0200

    Assignment 1 with corrections: Delete some comments; Delete unnesessary routers

--- node-express/routes/dishRouter.js
+++ node-express/routes/dishRouter.js
@@ -9,7 +9,8 @@ dishRouter.route('/')
 .all((req,res,next) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    next();
+    next(); //will pass the req res params for /dishes endpoint to next method,
+    //  which is in our case is app.get for /dishes
 })
 .get((req,res,next) => {
     res.end('Will send all the dishes to you!');
