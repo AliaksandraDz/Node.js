f3d0b7e93e281b8be067060c10082a950dd8033a
Alexandra <wilderweinundich@gmail.com>
2022-08-12 18:15:19 +0200

    Assignment 1: PromosRouter, LeadersRouter, DishIdRouter

--- node-express/index.js
+++ node-express/index.js
@@ -4,6 +4,8 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 const hostname = 'localhost';
 const port = 3000;
 
@@ -11,6 +13,11 @@ const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use('/dishes', dishRouter);
+app.use('/dishes/:dishId', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/promotions/:promoId', promoRouter);
+app.use('/leaders', leaderRouter);
+app.use('/leaders/:leaderId', leaderRouter);
 
 // app.all('/dishes', (req,res,next) => {
 //     res.statusCode = 200;
