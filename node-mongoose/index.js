3fa141639f12ec01f74c1e45f0a09520c28f0bc1
Alexandra <wilderweinundich@gmail.com>
2022-09-05 18:40:08 +0300

    Mongoose Part 3

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -17,7 +17,23 @@ connect.then((db) => {
         .then((dish) => {
             console.log(dish);
 
-            return Dishes.find({}).exec(); //Queries do return a thenable, but if you need a real Promise you should use the exec method. 
+            return Dishes.findByIdAndUpdate(dish._id, {
+                $set: { description: 'Updated test'}
+            },{ 
+                new: true //once the update of the dish is complete, then this will return the updated dish back to us
+            })
+            .exec(); //Queries do return a thenable, but if you need a real Promise you should use the exec method. 
+        })
+        .then((dish) => {
+            console.log(dish);
+    
+            dish.comments.push({
+                rating: 5,
+                comment: 'I\'m getting a sinking feeling!',
+                author: 'Leonardo di Carpaccio'
+            });
+    
+            return dish.save();
         })
         .then((dishes) => {
             console.log(dishes);
